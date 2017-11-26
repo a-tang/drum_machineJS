9dfac80d660056b6d3e572028f7bf6d32da3cad2
Alex Tang <alextang3@outlook.com>
2017-11-26 01:48:29 -0800

    Clicking the buttons now play the drums

--- javascript.js
+++ javascript.js
@@ -1,16 +1,31 @@
-  let removeTransition = e => {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing'); 
-  }
-  let playSound = e => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
+let removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing'); 
+}
+let playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`a[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+let playThis = (obj)=>{
+  const audioz = document.querySelector(`audio[data-key="${obj.getAttribute('data-key')}"]`);
+  const keyz = document.querySelector(`a[data-key="${obj.getAttribute('data-key')}"]`);
+  if (!audioz) return;
+  keyz.classList.add('playing');
+  audioz.currentTime = 0;
+  audioz.play(); 
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+window.onclick = function(e) {
+  if (!e.target.localName == 'a' || 'kbd' || 'span') return 
+    else {
+      clickFunction();
+    }
+  }
